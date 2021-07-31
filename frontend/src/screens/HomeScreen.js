bbc5d7ad72a88b7bd15dc0550625fefb57feb62b
sac.poddar <sac.poddar@gmail.com>
2021-07-31 20:40:32 +0530

    Fixed #7 sec5- redux: HomeScreen productListReducer and Action

--- frontend/src/screens/HomeScreen.js
+++ frontend/src/screens/HomeScreen.js
@@ -1,37 +1,52 @@
-import React, { useState, useEffect } from 'react'
+import React, { useEffect } from 'react'
+import { useDispatch, useSelector } from 'react-redux'
 import { Row, Col } from 'react-bootstrap'
 import Product from '../components/Product'
-import axios from 'axios'
+import { listProducts } from '../actions/productActions'
 
 const HomeScreen = () => {
   // useState - add local state to component
   // React will preserve this state between re-renders
   // useState returns a pair: the current state value and a function that lets you update it.
-  const [products, setProduct] = useState([])
+  // const [products, setProduct] = useState([])
+
+  // useDispatch - A hook to access the redux dispatch function.
+  const dispatch = useDispatch()
+
+  // useSelector - A hook to access the redux store's state.
+  const productList = useSelector((state) => state.productList)
+  const { loading, error, products } = productList
 
   // By using useEffect Hook, you tell React that your component needs to do something after render.
   // Data fetching, setting up a subscription, and manually changing the DOM in React components are
   // all examples of side effects or just effects
   useEffect(() => {
-    const fetchProducts = async () => {
-      const { data } = await axios.get('/api/products')
-      // call setProduct of useState
-      setProduct(data)
-    }
+    // const fetchProducts = async () => {
+    //   const { data } = await axios.get('/api/products')
+    //   // call setProduct of useState
+    //   setProduct(data)
+    // }
+    // fetchProducts()
 
-    fetchProducts()
-  }, [])
+    dispatch(listProducts())
+  }, [dispatch])
 
   return (
     <>
       <h1>Latest Products</h1>
-      <Row>
-        {products.map((product) => (
-          <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
-            <Product product={product}></Product>
-          </Col>
-        ))}
-      </Row>
+      {loading ? (
+        <h2>Loading...</h2>
+      ) : error ? (
+        <h3>{error}</h3>
+      ) : (
+        <Row>
+          {products.map((product) => (
+            <Col key={product._id} sm={12} md={6} lg={4} xl={3}>
+              <Product product={product}></Product>
+            </Col>
+          ))}
+        </Row>
+      )}
     </>
   )
 }
