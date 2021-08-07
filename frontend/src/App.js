7c5801fa27af3eb05da6e36f5545a8057c3a52de
sac.poddar <sac.poddar@gmail.com>
2021-08-07 14:13:10 +0530

    Fixed #10 sec6- CartScreen addToCart removeFromCart

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -5,20 +5,22 @@ import Header from './components/Header'
 import Footer from './components/Footer'
 import HomeScreen from './screens/HomeScreen'
 import ProductScreen from './screens/ProductScreen'
+import CartScreen from './screens/CartScreen'
 
 const App = () => {
   return (
     <Router>
-      <Header/>
+      <Header />
       <main className='py-3'>
         <Container>
           <Route path='/' component={HomeScreen} exact></Route>
           <Route path='/product/:id' component={ProductScreen}></Route>
+          <Route path='/cart/:id?' component={CartScreen}></Route>
         </Container>
       </main>
-      <Footer/>
+      <Footer />
     </Router>
-  );
+  )
 }
 
-export default App;
+export default App
