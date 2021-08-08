815c918c77ae6bf4ad34afe19ca64f72d806a8b3
sac.poddar <sac.poddar@gmail.com>
2021-08-08 18:16:40 +0530

    Fixed #12 sec7- backend user authentication and registration

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,7 @@ import dotenv from 'dotenv'
 import connectDB from './config/db.js'
 import colors from 'colors'
 import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
 import { errorHandler, notFound } from './middleware/errorMiddleware.js'
 
 dotenv.config()
@@ -11,11 +12,15 @@ connectDB()
 
 const app = express()
 
+// allow to accept JSON in body
+app.use(express.json())
+
 app.get('/', (req, res) => {
   res.send('API is running.. ')
 })
 
 app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
 
 // these will not be called if above route handlers complete resquest/response cycle
 app.use(notFound)
